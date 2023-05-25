614ecd21280f2c4d0fd9e445ccad14a81081b697
AyakaTakashima <takashima@catallaxy.me>
2023-05-25 23:03:06 +0900

    何回もテストが走ってしまう問題を解消

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -3,17 +3,13 @@
 require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
+  test 'should get index, pp and tos' do
     get top_url
     assert_response :success
-  end
 
-  test 'should get pp' do
     get pp_url
     assert_response :success
-  end
 
-  test 'should get tos' do
     get tos_url
     assert_response :success
   end
